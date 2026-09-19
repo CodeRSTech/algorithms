07fad3e2ff52a46a25021986161ecb2c1e55ee5e
CodeRSTech <codetechrs@gmail.com>
2026-09-19 14:50:15 +0530

    Initial code for Bubble sort

--- Brute Force/Bubble Sort/code.py
+++ Brute Force/Bubble Sort/code.py
@@ -1,1 +1,23 @@
-# TODO: Implement in Python
+from random import randint
+
+N = 15
+MIN = 1
+MAX = 20
+
+def BubbleSort(start:int, end:int, array:list):
+    newEnd:int = start;
+    for i in range(start, end):
+        if array[i] > array[i + 1]:
+            array[i], array[i+1] = array[i + 1], array[i]
+            newEnd = i
+
+    if newEnd == start:
+        return
+    else:
+        BubbleSort(start, newEnd, array)
+
+if __name__ == '__main__':
+    array = [randint(MIN, MAX) for _ in range(N)]
+    print("Initial Array: ", *array)
+    BubbleSort(0, N-1, array)
+    print("Sorted Array: ",*array)
