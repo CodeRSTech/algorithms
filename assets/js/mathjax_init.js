90e170298c9f409b795ff41bceeda3477a243ebc
CodeRSTech <codetechrs@gmail.com>
2026-09-20 12:32:51 +0530

    Create mathjax_init.js

---
+++ assets/js/mathjax_init.js
@@ -0,0 +1,12 @@
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [['$','$'], ['\\(','\\)']],
+      displayMath: [['$$','$$'], ['\\[','\\]']],
+      processEscapes: true
+    },
+    "HTML-CSS": {
+      styles: { ".MathJax": { "color": "#e0e0e0 !important" } }
+    }
+  });
+</script>
