b31127a2a3ede2646a4761dd1aa4560792041c14
CodeRSTech <codetechrs@gmail.com>
2026-09-20 23:28:47 +0530

    removed extra tags

--- assets/js/mathjax_init.js
+++ assets/js/mathjax_init.js
@@ -1,12 +1,10 @@
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({
-    tex2jax: {
-      inlineMath: [['$','$'], ['\\(','\\)']],
-      displayMath: [['$$','$$'], ['\\[','\\]']],
-      processEscapes: true
-    },
-    "HTML-CSS": {
-      styles: { ".MathJax": { "color": "#e0e0e0 !important" } }
-    }
-  });
-</script>
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [['$$','$$'], ['\\[','\\]']],
+    processEscapes: true
+  },
+  "HTML-CSS": {
+    styles: { ".MathJax": { "color": "#e0e0e0 !important" } }
+  }
+});
